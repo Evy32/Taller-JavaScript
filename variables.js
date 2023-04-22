1cbafd29e2b09554faa08d6f4900f27f1d6b3881
Evy32 <evbp080116@gmail.com>
2023-04-21 19:18:31 -0500

    Version 1.0

---
+++ variables.js
@@ -0,0 +1,21 @@
+let a = 23;
+let b = 36;
+
+if (a<b){
+    console.log("El numero menor es: " + a);
+}
+else{
+    console.log("El numero menor es: " + b);
+}
+
+var c = 52
+var d = 46
+var e = 39
+
+if(c>d && c>e){
+    console.log("El numero mayor es: " + c);
+}else if(d>e && d>c){
+    console.log("El numero mayor es: " + d);
+}else if(e>d && e>c){
+    console.log("El numero mayor es: " + e);
+}
